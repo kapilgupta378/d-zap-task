dc59f549650a1414a2d20610d675fe9f6a510f73
kapilgupta <kapilgupta>
2024-02-27 00:55:54 +0530

    created loading component

---
+++ src/design-systems/Molecules/Loading/StakeInfo/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface StakeInfoLoadingProps {}
+
+const StakeInfoLoading: React.FC<StakeInfoLoadingProps> = () => {
+  return (
+    <div className="w-[50%]">
+      <div className="mx-auto">
+        <div
+          style={{
+            background:
+              "linear-gradient(157deg, rgba(192, 0, 183, 0.60) -1.77%, rgba(115, 0, 204, 0.60) 16.09%, rgba(4, 4, 16, 0.00) 35.63%)",
+          }}
+          className="flex grow basis-[0%] flex-col px-14 py-11 rounded-[48px] max-md:max-w-full max-md:px-5"
+        >
+          <div className="w-full mx-auto">
+            <div className="flex grow basis-[0%] flex-col rounded-[48px] max-md:max-w-full max-md:px-5">
+              <div className="animate-pulse bg-neutral-400 h-14 w-32 mb-5 self-center"></div>
+              <div className="flex gap-5">
+                <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
+                <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
+              </div>
+              <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
+              <div className="flex justify-center gap-5">
+                <div className="animate-pulse bg-neutral-400 rounded-full h-10 w-40 self-center mb-8"></div>
+                <div className="animate-pulse bg-neutral-400 rounded-full h-10 w-40 self-center mb-8"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StakeInfoLoading;
--- src/design-systems/Organisms/StakeInfo/index.tsx
+++ src/design-systems/Organisms/StakeInfo/index.tsx
@@ -1,6 +1,8 @@
 import Button from "@/design-systems/Atoms/Button";
 import { Tooltip } from "@/design-systems/Atoms/ToolTip";
 import Typography from "@/design-systems/Atoms/Typography";
+import InfoCard from "@/design-systems/Molecules/Cards/InfoCard";
+import StakeInfoLoading from "@/design-systems/Molecules/Loading/StakeInfo";
 import { useClaimReward } from "@/hooks/ContractHooks/useClaimReward";
 import { useUnStake } from "@/hooks/ContractHooks/useStake";
 import { useStakerDetails } from "@/hooks/ContractHooks/useStakerDetails";
@@ -34,34 +36,7 @@ const StakeInfo = () => {
   };
 
   if (isLoading) {
-    return (
-      <div className="w-[50%]">
-        <div className=" mx-auto">
-          <div
-            style={{
-              background:
-                "linear-gradient(157deg, rgba(192, 0, 183, 0.60) -1.77%, rgba(115, 0, 204, 0.60) 16.09%, rgba(4, 4, 16, 0.00) 35.63%)",
-            }}
-            className="flex  grow basis-[0%] flex-col px-14 py-11 rounded-[48px] max-md:max-w-full max-md:px-5"
-          >
-            <div className="w-full  mx-auto">
-              <div className="flex  grow basis-[0%] flex-col  rounded-[48px] max-md:max-w-full max-md:px-5">
-                <div className="animate-pulse bg-neutral-400  h-14 w-32 mb-5 self-center"></div>
-                <div className="flex gap-5">
-                  <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
-                  <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
-                </div>
-                <div className="bg-neutral-400 animate-pulse rounded-2xl h-28 w-full self-stretch mb-8"></div>
-                <div className="flex justify-center gap-5">
-                  <div className="animate-pulse bg-neutral-400 rounded-full h-10 w-40 self-center mb-8"></div>
-                  <div className="animate-pulse bg-neutral-400 rounded-full h-10 w-40 self-center mb-8"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <StakeInfoLoading />;
   }
 
   return (
@@ -81,51 +56,20 @@ const StakeInfo = () => {
 
       <div className="flex gap-5 mt-12 mb-12 flex-col flex-wrap">
         <div className="flex gap-5  flex-row w-full">
-          <div className="bg-white  bg-opacity-10 backdrop-blur-152  self-stretch flex  justify-between gap-5 px-8 py-6 rounded-sm items-start max-md:max-w-full max-md:flex-wrap max-md:mt-10 max-md:px-5 w-[50%]">
-            <div className="flex grow basis-[0%] flex-col items-stretch w-full">
-              <div className="text-neutral-400 text-lg">Unclaimed Rewards</div>
-              <Tooltip
-                message={Number(stakerDetails.unclaimedRewards).toFixed(2)}
-              >
-                <Typography
-                  size="h2"
-                  className="text-white w-full truncate text-6xl font-semibold leading-10 mt-5 max-md:text-4xl max-md:leading-8"
-                >
-                  {Number(stakerDetails.unclaimedRewards).toFixed(2)}
-                </Typography>
-              </Tooltip>
-            </div>
-          </div>
-          <div className="bg-white bg-opacity-10 backdrop-blur-152  self-stretch flex  justify-between gap-5 px-8 py-6 rounded-sm items-start max-md:max-w-full max-md:flex-wrap max-md:mt-10 max-md:px-5 w-[50%]">
-            <div className="flex grow basis-[0%] flex-col items-stretch w-full">
-              <div className="text-neutral-400 text-lg">Total stack</div>
-              <Tooltip message={Number(stakerDetails.stakedAmount).toFixed(2)}>
-                <Typography
-                  size="h2"
-                  className="text-white !w-full truncate text-6xl font-semibold leading-10 mt-5 max-md:text-4xl max-md:leading-8"
-                >
-                  {Number(stakerDetails.stakedAmount).toFixed(2)}
-                </Typography>
-              </Tooltip>
-            </div>
-          </div>
+          <InfoCard
+            title="Unclaimed Rewards"
+            totalRewardsClaimed={stakerDetails.unclaimedRewards}
+          />
+          <InfoCard
+            title="Total stack"
+            totalRewardsClaimed={stakerDetails.stakedAmount}
+          />
         </div>
 
-        <div className="bg-white bg-opacity-10 backdrop-blur-152  flex justify-between gap-5 px-8 py-6 rounded-sm items-start  max-md:flex-wrap max-md:px-5 w-[50%]">
-          <div className="flex grow flex-col items-stretch w-full">
-            <div className="text-neutral-400 text-lg">Claimed Rewards</div>
-            <Tooltip
-              message={Number(stakerDetails.totalRewardsClaimed).toFixed(2)}
-            >
-              <Typography
-                size="h2"
-                className="text-white truncate !w-full  text-6xl font-semibold leading-10 mt-5 max-md:text-4xl max-md:leading-8"
-              >
-                {Number(stakerDetails.totalRewardsClaimed).toFixed(2)}
-              </Typography>
-            </Tooltip>
-          </div>
-        </div>
+        <InfoCard
+          title="Claimed Rewards"
+          totalRewardsClaimed={stakerDetails.totalRewardsClaimed}
+        />
       </div>
       <div className="flex gap-5 justify-center">
         <Button
